272943fe9b821c5a7ea239f39c68b41042216c1d
Abdelrhman <abdelrhmanabouelenan@gmail.com>
2023-03-26 23:03:02 +0200

    Lab3 Assignment

---
+++ main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      // A widget which will be started on application startup
+      home: MyHomePage(title: 'First Application'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(
+          'Abdelrhman Abouelenan',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
